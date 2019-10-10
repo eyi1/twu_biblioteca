726256e6776115b7163218e2918df63acdcad107
github username <ej7729@gmail.com>
2019-10-10 06:55:58 -0400

    display welcome message

---
+++ WelcomeMsg.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class WelcomeMsg {
+
+    public void displayWelcomeMsg(){
+        System.out.println("Welcome to Biblioteca! You're one stop shop for great book titles in Bangalore!");
+    }
+}

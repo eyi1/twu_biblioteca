1c18b49bd746c9db3313b789d0c160eacad24eb8
github username <ej7729@gmail.com>
2019-10-11 09:49:09 -0400

    user input method using scanner

--- UserInput.java
+++ UserInput.java
@@ -1,4 +1,15 @@
 package com.twu.biblioteca;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UserInput {
+
+    public static String userSelection(String prompt) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(prompt); //prompt
+        String message = s.nextLine();
+        System.out.println(message);
+        return message;
+    }
 }

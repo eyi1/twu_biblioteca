924737e674ef9b7beed5152c1871975e6019fcc3
github username <ej7729@gmail.com>
2019-10-11 09:46:56 -0400

    remove line break in string

--- WelcomeMsg.java
+++ WelcomeMsg.java
@@ -3,6 +3,6 @@ package com.twu.biblioteca;
 public class WelcomeMsg {
 
     public void displayWelcomeMsg(){
-        System.out.println("Welcome to Biblioteca! You're one stop shop for great book titles in Bangalore!");
+        System.out.println("Welcome to Biblioteca! Your one stop shop for great book titles in Bangalore!");
     }
 }
